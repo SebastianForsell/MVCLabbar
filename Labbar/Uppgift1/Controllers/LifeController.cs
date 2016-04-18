17d162107d7b1107004a8d3029d57e84d2ae9075
SebastianForsell <sebastian@brantevik.net>
2016-04-18 15:28:28 +0200

    Labb1 Klar

---
+++ Labbar/Uppgift1/Controllers/LifeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Uppgift1.Controllers
+{
+    public class LifeController : Controller
+    {
+        // GET: Life
+        public ActionResult SuperMario(int? id)
+        {
+            if (id != null)
+            {
+                ViewBag.GetHelp = "is using Microsoft.SharePoint :)";
+            }
+            return View();
+        }
+        public ActionResult Health()
+        {
+            return View();
+        }
+        public ActionResult Live()
+        {
+            return View();
+        }
+    }
+}
