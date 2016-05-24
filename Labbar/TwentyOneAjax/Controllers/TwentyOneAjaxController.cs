de7b5c51bfac24c8b71043640c8f3e6d79014940
SebastianForsell <sebastian@brantevik.net>
2016-05-24 15:26:59 +0200

    Twenty One Ajax version Labb 4 del 1 klar

---
+++ Labbar/TwentyOneAjax/Controllers/TwentyOneAjaxController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TwentyOneAjax.Models;
+
+namespace TwentyOneAjax.Controllers
+{
+    public class TwentyOneAjaxController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult _Game()
+        {
+            return PartialView(new TwentyOneAjaxModel());
+        }
+        
+        [HttpPost]
+        public PartialViewResult _Game(TwentyOneAjaxModel model)
+        {
+            model.IncreaseActualNumber(1);
+            model.ComputerChoice();
+            model.IncreaseActualNumber(2);
+            ModelState.Remove("ActualNumber");
+            ModelState.Remove("CpuTurn");
+            return PartialView(model);
+        }
+    }
+}
